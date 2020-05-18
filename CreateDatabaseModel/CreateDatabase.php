79781bf944f177769a884fdf2f648e50456ce71e
kstro96 <alejandrocastro9b@hotmail.com>
2020-05-17 21:27:58 -0500

    Creation of index to setup backend

---
+++ CreateDatabaseModel/CreateDatabase.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/titles.css">
+    <link rel="stylesheet" type="text/css" href="css/back.css">
+    <title>Creacion de base de datos</title>
+</head>
+
+<body>
+    <h1>Creación de base de datos</h1>
+    <h2>Proyecto Programación web</h2>
+    <h3>Mayo 2020</h3>
+    <div>
+
+    </div>
+    <?php
+    include_once dirname(__FILE__) . '/config/config.php';
+    $con = mysqli_connect(HOST_DB, USUARIO_DB, USUARIO_PASS);
+    $sql = "CREATE DATABASE " . DATABASE_NAME;
+    if (mysqli_query($con, $sql)) {
+        echo "<br><div class=\"result_query success_text\"> Base de datos " . DATABASE_NAME . " creada </div>";
+    } else {
+        echo "<br><div class=\"result_query error_text\"> Error en la creacion " . mysqli_error($con) . "</div>";
+    }
+    ?>
+    <br><br>
+    <div>
+        <a class="back" href="index.php">Regresar</a>
+    </div>
+
+
+</body>
+
+</html>
---
+++ CreateDatabaseModel/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/titles.css">
+    <link rel="stylesheet" type="text/css" href="css/indextable.css">
+    <title>Backend Creation</title>
+</head>
+
+<body>
+    <h1>Index para la creación del backend</h1>
+    <h2>Proyecto Programación web</h2>
+    <h3>Mayo 2020</h3>
+
+    <div class="indexTable">
+        <table class="indexTable">
+            <tbody>
+                <tr>
+                    <td>Creación base de datos</td>
+                    <td><a href="CreateDatabase.php">Link</a></td>
+                </tr>
+                <tr>
+                    <td>Gestor de archivos</td>
+                    <td><a href="gestorarchivos.php">Link</a></td>
+                </tr>
+            </tbody>
+            </tr>
+        </table>
+    </div>
+
+</body>
+
+</html>
