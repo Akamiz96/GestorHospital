9d995581ba06b1ea1fc61574057d426dcd2ce061
kstro96 <alejandrocastro9b@hotmail.com>
2020-05-20 15:04:50 -0500

    index.php login form without functionality

--- GestorHospital/index.php
+++ GestorHospital/index.php
@@ -5,12 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/titles.css">
-    <link rel="stylesheet" type="text/css" href="css/indextable.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap" rel="stylesheet">
     <title>Salud Sis</title>
 </head>
 
 <body>
-    <h1>Gestor del Hospital</h1>
+    <div class="title_page">
+        <h1>Gestor del Hospital</h1>
+    </div>
+
+    <div class="login">
+        <div class="login-form">
+            <h3>Username:</h3>
+            <input type="text" placeholder="Username" /><br>
+            <h3>Password:</h3>
+            <input type="password" placeholder="Password" />
+            <br><br>
+            <input type="button" value="Login" class="login-button" />
+            <br><br>
+            <a class="sign-up">Registro</a>
+        </div>
+    </div>
 
 </body>
 
