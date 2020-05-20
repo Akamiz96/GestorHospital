160ba48460eb37495bb891d1f69a9d2a8db41996
kstro96 <alejandrocastro9b@hotmail.com>
2020-05-20 17:45:25 -0500

    Prueba gitignore

--- GestorHospital/config/config.php
+++ GestorHospital/config/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('USUARIO_DB','alejokstro08');
+    define('USUARIO_DB','alejokstro96');
     define('USUARIO_PASS','EnwKMkpbZAlgROdB');
     define('HOST_DB','localhost');
     define('DATABASE_NAME', 'ProyectDatabase');
