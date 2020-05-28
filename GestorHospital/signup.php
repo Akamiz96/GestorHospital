587e54c10c09e2a52e11cc33d004b8ec41840d97
kstro96 <alejandrocastro9b@hotmail.com>
2020-05-28 15:30:46 -0500

    new start

--- GestorHospital/signup.php
+++ GestorHospital/signup.php
@@ -22,13 +22,46 @@
             <form action="index.php" method="POST">
                 <h3>Nombre de usuario:</h3>
                 <input type="text" placeholder="Nombre de usuario" /><br>
+                <?php
+                include_once dirname(__FILE__) . '/utils/validateform.php';
+                if (isset($_POST["username"])) {
+                    if (validateUserNameField($_POST["username"])) {
+                    } else {
+                        echo "<div class=\"error-message\">";
+                        echo "ERROR";
+                        echo "</div>";
+                    }
+                }
+                ?>
                 <h3>Email:</h3>
                 <input type="text" placeholder="Email" /><br>
                 <h3>Confirmar email:</h3>
                 <input type="text" placeholder="Confirmar email" /><br>
+                <?php
+                    include_once dirname(__FILE__) . '/utils/validateform.php';
+                    if (isset($_POST["password"])) {
+                        if (validatePasswordField($_POST["password"])) {
+                        } else {
+                            echo "<div class=\"error-message\">";
+                            echo "ERROR";
+                            echo "</div>";
+                        }
+                    }
+                ?>
                 <h3>Contraseña:</h3>
-                <input type="password" placeholder="Contraseña" />
-                <br><br>
+                <input type="password" placeholder="Contraseña" /><br>
+                <?php
+                include_once dirname(__FILE__) . '/utils/validateform.php';
+                if (isset($_POST["password"])) {
+                    if (validatePasswordField($_POST["password"])) {
+                    } else {
+                        echo "<div class=\"error-message\">";
+                        echo "ERROR";
+                        echo "</div>";
+                    }
+                }
+                ?>
+                <br>
                 <input type="submit" value="Registrarse" class="login-button" />
                 <br><br>
                 <a class="sign-up" href="index.php">Regresar</a>
